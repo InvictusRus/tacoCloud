763fee26b1d5803831e4693ea2be5d7f16923bab
zhia <zheronkin.i@artix.su>
2022-04-07 14:10:45 +0700

    TacoCloud

--- src/main/java/sia/tacocloud/DesignTacoController.java
+++ src/main/java/sia/tacocloud/DesignTacoController.java
@@ -2,11 +2,13 @@ package sia.tacocloud;
 
 import lombok.extern.slf4j.Slf4j;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,7 +63,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
 
